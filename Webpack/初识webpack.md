19cb5e8cfce077d4aab5dbe63e602cedc01e3477
776488326 <776488326@qq.com>
2022-05-04 16:38:47 +0800

    webpack

---
+++ Webpack/初识webpack.md
@@ -0,0 +1,16 @@
+# 初识webpack
+
+## 介绍
+
+- 前端工程化工具
+
+### 安装
+
+- 安装Node.js / Macos——》brew install node
+- 安装webpack / npm i webpack webpack-cli -D
+
+### 运行
+
+- 在需要打包的目录下运行cmd命令 npx webpack
+- 如果是全局安装的webpack，则可直接使用 webpack
+- 局部安装需要使用npx来查找webpack的安装位置
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const htmlwebpackplugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "入口文件",
+    output:{
+        filename: "打包生成的文件名",
+        path:path.resolve(__dirname,"./dist"),
+        clean: true, //打包前删除上次生成的打包文件
+        assetModuleFilename: "images/[contenthash][ext]" // 设置导入的资源文件夹，文件名，扩展名
+    },
+    mode: "development",//开发模式
+
+    devtool: "inline-source-map",//精准定位源文件错误位置
+
+    plugins: [
+        new htmlwebpackplugin({
+            template: "入口文件",
+            filename: "打包生成的文件名",
+            inject: "打包的js文件引入的位置"
+        })
+    ],
+
+    devServer: {  //实时更新
+        static: "./dist"
+    },
+
+    module:{
+        rules: [
+            {
+                test: /\.png$/,         //导出url
+                type: "asset/resource",
+                generator:{
+                    filename: "images/[contenthash][ext]" // 设置导入的资源文件夹，文件名，扩展名。优先级更高
+                }
+            },
+            {
+                test: /\.svg$/,         //导出data.url
+                type: "asset/inline",
+            },
+            {   
+                test: /\.txt$/,         //导出源代码
+                type: "asset/source",
+            },
+            {
+                test: /\.jpg$/,         //导出url或data.url
+                type: "asset"
+            },
+            {
+                test: /\.css$/,         //解析css
+                use: ["style-loader","css-loader"] //从右往左依次执行
+            }
+        ]
+    }
+}
